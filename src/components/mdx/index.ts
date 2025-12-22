f0e5a4f7992e4f07340dfe300e59a66c00919e20
madmti <supermatrix9@gmail.com>
2025-12-22 11:20:47 -0300

    feat: added components for CartaProyecto

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -5,7 +5,8 @@ import Heading from './Heading.astro';
 import Paragraph from './Paragraph.astro';
 import Accent from './Accent.astro';
 import Stat from './Stat.astro';
+import Badge from './Badge.astro';
 import ContribuirCard from './ContribuirCard.astro';
 import ContribuirStep from './ContribuirStep.astro';
 
-export { Mini, Strong, Sub, Heading, Paragraph, Accent, Stat, ContribuirCard, ContribuirStep };
+export { Mini, Strong, Sub, Heading, Paragraph, Accent, Stat, Badge, ContribuirCard, ContribuirStep };
